af0aff98dc7c31ba3678922c5b845c5617060b7b
zhanglijie01 <zhanglijie01@rouchi.com>
2021-03-27 20:05:32 +0800

    dev

--- pkg/app/jwt.go
+++ pkg/app/jwt.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/dgrijalva/jwt-go"
 	"my-blog-service-go/global"
+	"my-blog-service-go/pkg/util"
 	"time"
 )
 
@@ -17,9 +18,36 @@ func  GetJWTSecret() []byte  {
 	return []byte(global.JWTSetting.Secret)
 }
 
+//生成token
 func GenerateToken(appKey, appSecret string) (string, error) {
-	//nowTime := time.Now()
-	//expireTime := nowTime.Add(global.JWTSetting.Expire)
-	//claims := Claims{
+	nowTime := time.Now()
+	expireTime := nowTime.Add(global.JWTSetting.Expire)
+	claims := Claims{
+		AppKey: util.EncodeMD5(appKey),
+		AppSecret: util.EncodeMD5(appSecret),
+		StandardClaims : jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer: global.JWTSetting.Issuer,
+		},
 	}
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token, err := tokenClaims.SignedString(GetJWTSecret())
+	return token, err
+}
+
+//解析token
+func ParseToken(token string) (*Claims, error)  {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return GetJWTSecret(), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if tokenClaims != nil {
+		claims, ok := tokenClaims.Claims.(*Claims)
+		if ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+	return nil, err
 }
